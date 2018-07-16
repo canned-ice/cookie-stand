6d13a5dfdc036fb7303a3ebf935b68c57368a4ad
canned-ice <torenesk@gmail.com>
2018-07-16 14:35:04 -0700

    added object literals of stores (needs random)

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
 'use strict';
 
-var store = {
-  location: 'pike',
+// Declaring store location objects
+var pikeStore = {
   displayName: '1st and Pike',
   minCustomerHour: 23,
   maxCustomerHour: 65,
   avgCookies: 6.3
 };
-
+var seatacStore = {
+  displayName: 'SeaTac Airport',
+  minCustomerHour: 3,
+  maxCustomerHour: 24,
+  avgCookies: 1.2
+};
+var centerStore = {
+  displayName: 'Seattle Center',
+  minCustomerHour: 11,
+  maxCustomerHour: 38,
+  avgCookies: 3.7
+};
+var caphillStore = {
+  displayName: 'Capitol Hill',
+  minCustomerHour: 20,
+  maxCustomerHour: 38,
+  avgCookies: 2.3
+};
+var alkiStore = {
+  displayName: 'Alki',
+  minCustomerHour: 2,
+  maxCustomerHour: 16,
+  avgCookies: 4.6
+};
