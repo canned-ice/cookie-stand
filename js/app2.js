7e94ea8b256c7ac34f8d81d98485584e1cf4912f
canned-ice <torenesk@gmail.com>
2018-07-17 11:22:33 -0700

    working on constructors

--- js/app2.js
+++ js/app2.js
@@ -1,11 +1,17 @@
 'use strict';
 
-function Store(name, displayName, minCustomerHour, maxCustomerHour, avgCookies, openHours, salesHourly) {
-  this.name = name; // instance name
+var allStores = [];
+
+function Store(displayName, minCustomerHour, maxCustomerHour, avgCookies, openHours, salesHourly) {
   this.displayName = displayName;
   this.minCustomerHour = minCustomerHour;
   this.maxCustomerHour = maxCustomerHour;
   this.avgCookies = avgCookies;
   this.openHours = openHours;
   this.salesHourly = salesHourly;
+
+  allStores.push(this);
 }
+var pike = new Store('1st and Pike', 23, 65, 6.3, ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'], []);
+console.log(pike);
+console.log(allStores);
